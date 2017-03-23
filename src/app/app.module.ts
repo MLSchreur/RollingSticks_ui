a59a8d358176be360288c5f27e047c58febcab72
MLSchreur <mschreur@zeelandnet.nl>
2017-03-23 14:09:48 +0100

    login screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule }             from '@angular/http';
 
 import { AppComponent }           from './app.component';
 import { BladmuziekComponent }    from './bladmuziek/bladmuziek.component';
+import { LoginComponent }         from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BladmuziekComponent
+    BladmuziekComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
