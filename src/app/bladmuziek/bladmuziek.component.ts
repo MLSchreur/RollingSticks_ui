def0e376a09a477ca1f947d2ec590c1c0872c5c8
Rebaf <Cor.Faber@gmail.com>
2017-03-23 14:58:56 +0100

    GET requests (All en ById) geïmplementeerd.

--- src/app/bladmuziek/bladmuziek.component.ts
+++ src/app/bladmuziek/bladmuziek.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
 
+import { Bladmuziek }            from './bladmuziek';
+import { BladmuziekService }     from './bladmuziek.service';
+
+
 @Component({
   selector: 'my-bladmuziek',
   templateUrl: './bladmuziek.component.html',
+  providers:[ BladmuziekService ]
 })
+
 export class BladmuziekComponent {
   title = 'bladmuziek works!';
+  allBladmuziek:  Bladmuziek[];
+  bladmuziek:     Bladmuziek;
+  bladmuziekId:   number;
+
+  constructor(private bladmuziekService: BladmuziekService) {
+  }
+
+  getBladmuziek() {
+    this.bladmuziekService.getBladmuziek().subscribe(allBladmuziek => {
+      console.log("Bladmuziek alles, succes!");
+      console.log(allBladmuziek);
+      this.allBladmuziek = allBladmuziek;
+    });
+  }
+
+  getBladmuziekById(id: number) {
+    console.log(this.bladmuziekId);
+    console.log(id);
+    this.bladmuziekService.getBladmuziekById(id).subscribe(bladmuziek => {
+      console.log("Bladmuziek per stuk, succes!");
+      console.log(bladmuziek);
+      this.bladmuziek = bladmuziek;
+    });
+  }
 }
