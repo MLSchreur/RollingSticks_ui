3039a50248601baca4942e3c7be494f421379fa6
Rebaf <Cor.Faber@gmail.com>
2017-03-23 10:35:21 +0100

    1e opzet frontend server incl bladmuziek.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule }          from '@angular/platform-browser';
+import { NgModule }               from '@angular/core';
+import { FormsModule }            from '@angular/forms';
+import { HttpModule }             from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }           from './app.component';
+import { BladmuziekComponent }    from './bladmuziek/bladmuziek.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BladmuziekComponent
   ],
   imports: [
     BrowserModule,
