ef089de5d5fe71152c6f42d31021900cb077d773
FaerieRose <FaerieRose@users.noreply.github.com>
2017-03-29 20:15:51 +0200

    Delete logout.component.ts

--- src/app/Login/logout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component }    from '@angular/core';
-import { Router }       from '@angular/router';
-
-@Component({
-  selector: 'logout',
-  templateUrl: './logout.component.html',
-})
-export class LogoutComponent {
-}
