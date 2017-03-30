0775107581ff51603b84dc0733a553ee1e6e6dd4
iamglicious <gregory.vrede@ia-events.com>
2017-03-30 15:07:26 +0200

    Routing toegevoegd muziekupload
    Muziek upload routing toegevoegd bij docenten component en docent
    pagina aangepast

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,25 +9,25 @@ import { LogoutComponent }               from "app/login/logout.component";
 import { LoginComponent }                from "app/login/login.component";
 import { LeerlingAanmakenComponent }     from "app/docent/leerlingaanmaken.component";
 import { VerzoekComponent }              from "app/verzoek/verzoek.component";
-// voor Observable is deze import nodig. Het bestand rxjs-extenstions.ts moet in dezelfde map staan als waar de app.module.ts staat (app).
-// Later import verplaatsen na implementeren van routing.
+import { MuziekstukComponent }           from "app/muziekstuk/muziekstuk.component";
 import './rxjs-extensions';
-
 
 // Route Confiratie van de menu's
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home'    , component: HomeComponent },
-  { path: 'leerling', component: LeerlingComponent },
-  { path: 'login'   , component: LoginComponent },
-  { path: 'loguit'  , component: LogoutComponent },
-  { path: 'verzoek'  , component: VerzoekComponent },
+  { path: 'home'    , component: HomeComponent      },
+  { path: 'leerling', component: LeerlingComponent  },
+  { path: 'login'   , component: LoginComponent     },
+  { path: 'loguit'  , component: LogoutComponent    },
+  { path: 'verzoek' , component: VerzoekComponent   },
+  { path: 'verzoek' , component: MuziekstukComponent   },
   { path: 'docent'  , component: DocentComponent,  children: [
-      { path: '',                   component: DocentComponent},
-      { path: 'leerlingoverzicht',  component: LeerlingOverzichtComponent},
-      { path: 'leerlingaanmaken',   component: LeerlingAanmakenComponent}
+      // { path: '',                   component: DocentComponent},
+      { path: 'leerlingoverzicht',   component: LeerlingOverzichtComponent},
+      { path: 'leerlingaanmaken' ,   component: LeerlingAanmakenComponent},
+      { path: 'muziekupload'          ,   component: MuziekstukComponent   },
   ]},
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: '**'                   , redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
