f9683bc9af041fc9d182fcf3eb68a2ff3e1391a4
FaerieRose <rosalynn5772-git@yahoo.com>
2017-03-30 09:08:53 +0200

    Iets is leerlingservice

--- src/app/leerling/leerling.service.ts
+++ src/app/leerling/leerling.service.ts
@@ -6,7 +6,9 @@ import { Leerling }                 from '../Leerling/Leerling';
 
 @Injectable()
 export class LeerlingService {
-  constructor(private http: Http) {}
+  constructor(private http: Http) {
+    this.defineBaseUrl();
+  }
 
   private headers = new Headers({ 'Content-Type': 'application/json' });  
 //  private baseUrl = "http://rollingsticks.test.carpago.nl/api/leerling";  
@@ -19,4 +21,16 @@ export class LeerlingService {
   getLeerlingen(): Observable<Leerling[]> {
     return this.http.get(this.baseUrl).map(res => res.json());
   }
+
+  private defineBaseUrl() {
+    let hostName: string = window.location.hostname;
+    console.log(hostName);
+    if (hostName.substring(0,5) == "local") {
+      this.baseUrl = "http://localhost:8082/api/leerling";
+    } else {
+      this.baseUrl = "http://rollingsticks.test.carpago.nl/api/leerling";
+    }
+    console.log("Base url: " + this.baseUrl);
+  }
+  
 }
