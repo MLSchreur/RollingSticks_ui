edaab9caf7311ff7f90b0b48d013b2155699f48c
FaerieRose <rosalynn5772-git@yahoo.com>
2017-04-11 09:00:52 +0200

    navigatie aangepast met minimale beveiliging

--- src/app/home/navigatie.component.ts
+++ src/app/home/navigatie.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component }         from  '@angular/core';
+import { Router }            from  '@angular/router';
+import { AppGlobalService }  from '../app.global.service';
 
 @Component({
   selector:               'navigatie',
   templateUrl:            './navigatie.component.html',
+  styles:                 [`
+    .nav-link {
+      cursor: pointer;
+    }
+  `]
 })
 export class NavigatieComponent {
   title = 'navigatie';
 
+  constructor(private router: Router, private appGlobalService: AppGlobalService) {
+  }
+
+  checkHome() {
+    switch (this.appGlobalService.getLoginStatus()) {
+      case 2: this.router.navigate(['docent']); break;
+      case 1: this.router.navigate(['leerling']); break;
+      default: this.router.navigate(['']);
+    }
+  }
+
+  checkDocent() {
+    if (this.appGlobalService.getLoginStatus() == 2) {
+      this.router.navigate(['docent']);
+    }
+  }
+
+  checkLeerling() {
+    if (this.appGlobalService.getLoginStatus() >= 1) {
+      this.router.navigate(['leerling']);
+    }
+  }
+
+  checkLoguit() {
+    if (this.appGlobalService.getLoginStatus() >= 1) {
+      this.router.navigate(['loguit']);
+    }
+  }
 }
 
 
