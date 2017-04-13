c994879c9708b76abf3c439b808e780ead2d5505
MLSchreur <mschreur@zeelandnet.nl>
2017-04-13 19:00:04 +0200

    foutcodes afgerond

--- src/app/leerling/leerling.component.ts
+++ src/app/leerling/leerling.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit }    from '@angular/core';
-import { NotenbalkComponent }   from '../notenbalk/notenbalk.component';
+import { Component, OnInit }  from '@angular/core';
+import { NotenbalkComponent } from '../notenbalk/notenbalk.component';
 
-import { LeerlingService }      from './leerling.service';
-import { Leerling }             from './leerling';
-import { MuziekstukService }    from '../muziekstuk/muziekstuk.service';
-import { Muziekstuk }           from '../muziekstuk/muziekstuk';
-import { AppGlobalService }     from '../app.global.service';
-import { CompositieService }    from '../compositie/compositie.service';
-import { Compositie }           from '../compositie/compositie';
+import { LeerlingService }    from './leerling.service';
+import { Leerling }           from './leerling';
+import { MuziekstukService }  from '../muziekstuk/muziekstuk.service';
+import { Muziekstuk }         from '../muziekstuk/muziekstuk';
+import { AppGlobalService }   from '../app.global.service';
+import { CompositieService }  from '../compositie/compositie.service';
+import { Compositie }         from '../compositie/compositie';
 
 
 
@@ -16,16 +16,23 @@ import { Compositie }           from '../compositie/compositie';
   templateUrl: './leerling.component.html',
   providers: [CompositieService, LeerlingService, MuziekstukService]
 })
+
 export class LeerlingComponent implements OnInit {
+  
   title = 'leerling';
+
   leerlingen        : Leerling[] = [];
   leerlingSelected  : Leerling;
   allMuziekstuk     : Muziekstuk[];
   tempTxt           : String;
   tempCompositieTxt : string;
 
-  
-  constructor(private compositieService: CompositieService, private leerlingService: LeerlingService, private muziekstukService: MuziekstukService, private appGlobalService: AppGlobalService) {
+
+  constructor(
+    private compositieService: CompositieService,
+    private leerlingService: LeerlingService,
+    private muziekstukService: MuziekstukService,
+    private appGlobalService: AppGlobalService) {
   }
 
   ngOnInit() {
@@ -44,16 +51,15 @@ export class LeerlingComponent implements OnInit {
     this.leerlingService.getLeerlingById($event.target.value).subscribe(data => this.leerlingSelected = data);
   }
 
-  showCompositie(muziekstuk: Muziekstuk){
+  showCompositie(muziekstuk: Muziekstuk) {
     console.log(muziekstuk)
     console.log(muziekstuk.id)
-    // this.tempTxt = "ID: " + muziekstuk.id + "*** Artiest: " + muziekstuk.artiest + "*** Titel: " + muziekstuk.artiest;
     this.compositieService.parseXml(muziekstuk.id).subscribe(compositie => {
       console.log("Compositie, succes!");
       console.log(compositie);
-      if (typeof compositie === 'string'){
+      if (typeof compositie === 'string') {
         console.log("het is een string");
-        let tempcomp: string = compositie;
+//        let tempcomp: string = compositie;      //niet nodig??
         if (compositie == "1") {
           this.tempCompositieTxt = "Muziekstuk met opgegeven id bestaat niet";
         } else if (compositie == "2") {
@@ -63,7 +69,6 @@ export class LeerlingComponent implements OnInit {
         }
       } else {
         this.tempCompositieTxt = muziekstuk.artiest + " - " + muziekstuk.titel + " *** " + compositie.title;
-    
       }
       console.log("het is een fout");
     });
