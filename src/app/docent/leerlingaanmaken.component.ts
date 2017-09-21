9ce9ca3bacafce3b7b6119115caa90c01432951b
Marcel Schreur <mschreur@zeelandnet.nl>
2017-09-21 15:08:54 +0200

    toevoegen leerling/docent (nog niet gereed)

--- src/app/docent/leerlingaanmaken.component.ts
+++ src/app/docent/leerlingaanmaken.component.ts
@@ -1,7 +1,7 @@
 import { Component }                from '@angular/core';
 import { Leerling }                 from '../leerling/leerling';
-import { Docent }                 from '../docent/docent';
-import { LeerlingaanmakenService } from './leerlingaanmaken.service';
+import { Docent }                   from '../docent/docent';
+import { LeerlingaanmakenService }  from './leerlingaanmaken.service';
 
 @Component({
   selector:                     'leerlingaanmaken',
@@ -11,11 +11,30 @@ import { LeerlingaanmakenService } from './leerlingaanmaken.service';
 
 export class LeerlingAanmakenComponent {
 
-    leerlingAanmaken  : Leerling = new Leerling();
-    docentAanmaken    : Docent = new Docent();
-    leerlingId        : Number;
+  leerlingAanmaken  : Leerling = new Leerling();
+  docentAanmaken    : Docent = new Docent();
+  leerlingId        : Number;
 
-constructor(private leerlingaanmakenService: LeerlingaanmakenService) { }
+  constructor(private leerlingaanmakenService: LeerlingaanmakenService) { }
+
+  checkGebruiker(){
+    let checkLeerling = document.getElementById("radio-leerling");
+    let checkDocent = document.getElementById("radio-docent");
+    console.log("Radio checkbox leerling is: " + checkLeerling.getAttributeNode("checked").value);
+    console.log("Radio checkbox leerling is: " + checkDocent.getAttributeNode("checked").value);
+    // if (check.getAttributeNode("checked").value != "checked"){
+    //   this.leerlingAanmaken = this.docentAanmaken;
+    // }
+  }
+
+  // postGebruiker(){
+  //   this.checkGebruiker();
+  //   console.log(this.leerlingAanmaken);
+  //   this.leerlingaanmakenService.postLeerling(this.leerlingAanmaken).subscribe(leerlingId => {
+  //       console.log("Leerling aangemaakt, succes! - " + leerlingId);
+  //       this.leerlingId = +leerlingId;
+  //   });
+  // }
 
   postLeerling(){
     console.log(this.leerlingAanmaken);
